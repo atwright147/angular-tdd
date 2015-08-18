4d7efe760258321b51fa6faad131ade3f0556447
Andy Wright <atwright147@gmail.com>
2015-08-18 19:09:52 +0100

    setup

---
+++ test/firstSpec.js
@@ -0,0 +1,15 @@
+(function() {
+	'use strict'
+
+	describe('javascript', function() {
+		it('should know 2 + 2 is 4', function() {
+			expect(2 + 2).toEqual(4);
+		});
+	});
+
+	describe('Directive', function() {
+		it('should add attribute to element', function() {
+			// test goes here
+		});
+	});
+})();
